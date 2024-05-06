025f19ca1a4800f069718f6144d05b6c8a7a1911
vivavy <146124993+vivavy@users.noreply.github.com>
2024-05-06 23:15:08 +0300

    Update install.py

--- install.py
+++ install.py
@@ -6,11 +6,12 @@ if sys.platfort.find("linux") == -1:
 	print("Vi Software Manager designed only for linux, sorry")
 	sys.exit(1)
 
+if os.getuid():
+	print("Run me as root!")
+
 shutil.copytree(os.getcwd(), os.path.expanduser("~/.software"))
 
-with open(os.path.expanduser("~/.bashrc"), "at") as f:
-	f.write("export PATH=\"$HOME/.software/bin:$PATH\"\nexport SW=\"$HOME/.software\"")
-
-os.system("source ~/.bashrc")
+for file in os.listdir(os.path.expanduser("~/.software/bin")):
+	os.system(f"ln -s {os.path.expanduser('~/.software/bin/'+file} /usr/bin/{file}")
 
 print("Done, you can delete this directory")
