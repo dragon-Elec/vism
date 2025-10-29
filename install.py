9f776942bbab1ce50eecd7809c7b2e44863aa232
dragon-Elec <dragonroarholy@gmail.com>
2025-10-29 11:16:40 +0530

    Add config check rename setup prompt adjust install dir
    - bin/list: Added check for missing config file with exit message.
    - bin/setup: Changed prompt from "Directory: " to "App folder name: ".
    - install.py: Adjusted symlink target to /usr/local/bin for binaries.

--- install.py
+++ install.py
@@ -2,16 +2,19 @@
 
 import os, sys, shutil
 
-if sys.platfort.find("linux") == -1:
-	print("Vi Software Manager designed only for linux, sorry")
-	sys.exit(1)
+if sys.platform.find("linux") == -1:
+    print("Vi Software Manager designed only for linux, sorry")
+    sys.exit(1)
 
 if os.getuid():
-	print("Run me as root!")
+    print("Run me as root!")
+    sys.exit(1)
 
-shutil.copytree(os.getcwd(), os.path.expanduser("~/.software"))
+shutil.copytree(os.getcwd(), os.path.expanduser("~/.software"), dirs_exist_ok=True)
 
 for file in os.listdir(os.path.expanduser("~/.software/bin")):
-	os.system(f"ln -s {os.path.expanduser('~/.software/bin/'+file} /usr/bin/{file}")
+    os.system(
+        f"ln -s {os.path.expanduser('~/.software/bin/' + file)} /usr/local/bin/{file}"
+    )
 
 print("Done, you can delete this directory")
